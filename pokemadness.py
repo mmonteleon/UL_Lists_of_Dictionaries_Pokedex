b0d0a76aefb1d4cf3cb08f7e363bb7ef82db8b16
EC2 Default User <ec2-user@ip-172-31-34-164.us-east-2.compute.internal>
2019-07-11 11:59:46 +0000

    first commit

---
+++ pokemadness.py
@@ -0,0 +1,38 @@
+from pokedex import pokedex
+
+# Pokedex is a LIST of Pokemon DICTIONARIES.  
+# Each dictionary contains the keys:
+# index, name, attack, type, height and weight
+
+################################################################
+# 1 Print the first Pokemon dictionary in the Pokedex list.
+
+
+
+################################################################
+# 2 Print all the indices and names of the Pokemon in the list.
+
+
+
+
+################################################################
+# 3a Define a function that gets a Pokemon's name given it's index
+
+
+
+# 3b Test your function by asking the user to input a number and print
+# the matching Pokemon's name.
+
+
+################################################################
+# 4 Define a function that gets a Pokemon's index given it's name
+
+
+# 4b Test your function by asking the user to input a name and
+# printing the matching Pokemon's number
+# NOTE: The case must match.  "pikachu" will not work but "Pikachu" will.
+
+
+
+
+################################################################
